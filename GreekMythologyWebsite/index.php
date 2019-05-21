f71c05ca93ca221a8f174178eb4eb038496afbb8
Maggie <magi.nikolova@gmail.com>
2019-05-21 21:55:26 +0300

    Update index.php

--- GreekMythologyWebsite/index.php
+++ GreekMythologyWebsite/index.php
@@ -1,11 +1,13 @@
-/*
+<!--
+PHP Database Project
 
+New Bulgarian University
+PHP Course
+Winter semester 2018/2019
 
+author: Magdalena Nikolova
+-->
 
-
-*/
-
-asdasdasd
 <?php include 'header.php'; ?>
 <?php include 'content.php'; ?>
 
